afb67c15b6c420f4992f6006ff173dce2a7ebd80
Naufal <naufalrzld@gmail.com>
2021-08-08 17:32:20 +0700

    init commit

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const { NotePayloadSchema } = require("./schema")
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResul = NotePayloadSchema.validate(payload);
+    if (validationResul.error) {
+      throw new InvariantError(validationResul.error.message);
+    }
+  }
+};
+
+module.exports = NotesValidator
